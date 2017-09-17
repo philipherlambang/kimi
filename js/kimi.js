9eb9073b3c5dc0bde9e0f43070f92626e350bdb9
philipherlambang <philip.herlambang@gmail.com>
2017-09-18 00:56:52 +0700

    fix broken links and add tipue search

--- js/kimi.js
+++ js/kimi.js
@@ -1,60 +1,67 @@
-// basic toggle navigation menu
+$(function () {
+    // basic toggle navigation menu
 
-$('#buttonOpenSearchbar').on('click', function(){
-    $('#helpText, #buttonOpenSearchbar').hide();
-    $('#searchbar, #buttonCloseSearchbar').show();
-});
+    $('#buttonOpenSearchbar').on('click', function(){
+        $('#helpText, #buttonOpenSearchbar').hide();
+        $('#searchbar, #buttonCloseSearchbar').show();
+    });
 
-$('#buttonCloseSearchbar').on('click', function(){
-    $('#helpText, #buttonOpenSearchbar').show();
-    $('#searchbar, #buttonCloseSearchbar').hide();
+    $('#buttonCloseSearchbar').on('click', function(){
+        $('#helpText, #buttonOpenSearchbar').show();
+        $('#searchbar, #buttonCloseSearchbar').hide();
 
-});
+    });
 
-$('#buttonOpenSearchbarMobile').on('click', function () {
-    $('.fly-searchbar').show();
-});
+    $('#buttonOpenSearchbarMobile').on('click', function () {
+        $('.fly-searchbar').show();
+    });
 
-$('#buttonCloseSearchbarMobile').on('click', function(){
-    $('.fly-searchbar').hide();
-});
+    $('#buttonCloseSearchbarMobile').on('click', function(){
+        $('.fly-searchbar').hide();
+    });
+
+    //tooltips
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // tipue search
+        $('#tipue_search_input').tipuesearch();
+    });
 
 
 // firing owl carousel
 
-$('.homepage-slider').owlCarousel({
-    loop:false,
-    margin:10,
-    // nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+    $('.homepage-slider').owlCarousel({
+        loop:false,
+        margin:10,
+        // nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
         }
-    }
-});
+    });
 
 
-$('.slider-shop').owlCarousel({
-    loop:false,
-    margin:10,
-    // nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:3
+    $('.slider-shop').owlCarousel({
+        loop:false,
+        margin:10,
+        // nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
         }
-    }
-});
-
+    });
 
