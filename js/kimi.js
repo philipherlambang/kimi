7b56ec44233e64f8980051a1231ad6afc27529da
philipherlambang <philip.herlambang@gmail.com>
2017-09-22 10:48:31 +0700

    tag in homepage use owl carousel as well

--- js/kimi.js
+++ js/kimi.js
@@ -59,19 +59,19 @@ $('#imageMetainfo').on('click', function(){
     });
 
 
-    $('.slider-shop').owlCarousel({
-        loop:false,
+    $('.small-slider').owlCarousel({
+        loop:true,
         margin:10,
-        // nav:true,
+        dots:false,
         responsive:{
             0:{
-                items:1
+                items:4
             },
             600:{
-                items:1
+                items:4
             },
             1000:{
-                items:3
+                items:8
             }
         }
     });
